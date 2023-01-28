baeda38d7245d13f941f6e4fbe19d754e6638d02
rhemanth399 <120007414+rhemanth399@users.noreply.github.com>
2023-01-28 19:08:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,38 @@
-const app = require('./index.js');
+const timerButtons = document.querySelectorAll('.timer__button');
+const displayTimeLeft = document.querySelector('.display__time-left');
+const displayEndTime = document.querySelector('.display__end-time');
+const customForm = document.querySelector('#custom');
+let intervalId;
 
-app.listen(3000, () => {
-  console.log('server started');
+function timer(seconds) {
+    clearInterval(intervalId);
+
+    const now = Date.now();
+    const then = now + seconds * 1000;
+    displayTimeLeft.textContent = new Date(then - Date.now()).toISOString().substr(14, 5);
+    displayEndTime.textContent = new Date(then).toLocaleString();
+
+    intervalId = setInterval(() => {
+        const secondsLeft = Math.round((then - Date.now()) / 1000);
+        if (secondsLeft < 0) {
+            clearInterval(intervalId);
+            return;
+        }
+        displayTimeLeft.textContent = new Date(secondsLeft * 1000).toISOString().substr(14, 5);
+    }, 1000);
+}
+
+function startTimer() {
+    const seconds = parseInt(this.dataset.time);
+    timer(seconds);
+}
+
+timerButtons.forEach(button => button.addEventListener('click', startTimer));
+
+customForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const mins = this.minutes.value;
+    timer(mins * 60);
+    this.reset();
 });
+
